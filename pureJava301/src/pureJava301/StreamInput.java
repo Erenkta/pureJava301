0a68b989ec3e99fdec87b8627fbb0e9b7a0d7193
Eren Nokta <eren.nkta@gmail.com>
2023-07-17 11:26:47 +0300

    Input Output Stream commited
    Baska bir dosyadan okuduğumuz veriyi diğer bir dosyaya yazdık

--- pureJava301/src/pureJava301/StreamInput.java
+++ pureJava301/src/pureJava301/StreamInput.java
@@ -3,20 +3,26 @@ package pureJava301;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class StreamInput {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File file = new File("hello.txt");
+		File fileOutput = new File("hello output.txt");
 		InputStream inputStream = null;
+		OutputStream outpuStream = null;
 		try {
 			inputStream = new FileInputStream(file);
+			outpuStream = new FileOutputStream(fileOutput);
 			int c = 0;
 			while( (c = inputStream.read()) != -1 ) {
-				System.out.print( (char) c );
+				char ch = (char) c;
+				outpuStream.write(ch);
 			}
 		} catch (FileNotFoundException e) {
 			// TODO: handle exception
